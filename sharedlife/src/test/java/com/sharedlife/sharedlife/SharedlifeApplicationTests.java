a2db5eb5c6e0085c8e03b83aa3c8246e4836a03b
dishikin <dishikin1997@gmail.com>
2022-02-12 11:47:39 +0900

    commit 12/02/22

---
+++ sharedlife/src/test/java/com/sharedlife/sharedlife/SharedlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharedlife.sharedlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
